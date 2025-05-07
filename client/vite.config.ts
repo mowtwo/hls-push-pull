99e0c2568922f495d11251bee1dd20193fcc8082
1046099511 <1046099511@qq.com>
2025-05-07 11:01:45 +0800

    Refactor code structure for improved readability and maintainability

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import unocss from 'unocss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [unocss(), vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5556',
+        changeOrigin: true,
+      }
+    }
+  }
 })
